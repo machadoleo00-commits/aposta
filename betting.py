78a3c28c68257e4344bc49194453a629120d9caf
machadoleo00-commits <machado.leo00@gmail.com>
2026-08-13 16:06:09 -0300

    feat: adiciona carteira e limite de apostas

--- betting.py
+++ betting.py
@@ -2,5 +2,21 @@ import random
 cor=['verde','vermelho']
 chances = [6,4]
 carteira = 1000
-sorteio = random.choices(cor, weights=chances, k=1)
-print(f'a cor sorteada foi {sorteio}')
+while carteira > 500:
+    sorteio = random.choices(cor, weights=chances, k=1)[0]
+    sorteio = str(sorteio)
+    print(f'\n Você possui {carteira}$')
+    valor_apostado = input(f'\nQual valor deseja apostar? para desistir digite sair ')
+    if (valor_apostado).lower() == 'sair':
+        break
+    cor_apostada = str(input(f'\nEm qual cor deseja apostar? Verde ou Vermelho :'))
+    if sorteio == cor_apostada.lower():
+            print(f'\na cor sorteada foi {sorteio}! Parabens você ganhou {valor_apostado}')
+            carteira = carteira + int(valor_apostado)
+    elif sorteio != cor_apostada.lower():
+            print(f'\na cor sorteada foi {sorteio}! Que pena você perdeu {valor_apostado}')
+            carteira = carteira - int(valor_apostado)
+print(f"\nseu saldo final é de {carteira}$")
+
+
+
